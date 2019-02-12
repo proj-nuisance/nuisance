2c3cebbf362f6eaf3b293c339d01e8fc67854441
Christopher P. Cheng <forever.21@dartmouth.edu>
2019-02-12 14:56:04 -0500

    added CSV, ShimSetting

--- code/process_QA_metrics.py
+++ code/process_QA_metrics.py
@@ -98,26 +98,34 @@ def qa_metric_producer(source, output_csv):
     destination = open(output_csv, "a")
     product = csv.writer(destination)
     
-    product.writerow(["Date", "Filetype", "tsnr", "SAR", "AcquisitionTime", "TxRefAmp", "SoftwareVersions"])
+    product.writerow(["Date", "Filetype", "tsnr", "SAR", "AcquisitionTime", "TxRefAmp", "SoftwareVersions", "CSV", "RepetitionTime", 
+        "Shim1", "Shim2", "Shim3", "Shim4", "Shim5", "Shim6", "Shim7", "Shim8"])
 
     for item in source:  # os.listdir(os.fsencode("derivatives")):
         info = re.search('.*/ses-(?P<date>[0-9]+)/.*', item).groupdict()
         sesame = open(os.fsdecode(item), "r").read()  # open sesameeee
         greetings = json.loads(sesame)
         bids_meta = greetings["bids_meta"]
+        shim = bids_meta["ShimSetting"]
+
+        print(item); # debugging
 
         # 2018 and later conditions
         if 'SAR' and "AcquisitionTime" and "TxRefAmp" in greetings:
             product.writerow([info["date"],
                 os.fsdecode(item)[59:], greetings["tsnr"], greetings["SAR"],
-                greetings["AcquisitionTime"], greetings["TxRefAmp"], bids_meta["SoftwareVersions"]])
+                greetings["AcquisitionTime"], greetings["TxRefAmp"], bids_meta["SoftwareVersions"],
+                bids_meta["ConversionSoftwareVersion"], bids_meta["RepetitionTime"], 
+                shim[0], shim[1], shim[2], shim[3], shim[4], shim[5], shim[6], shim[7]])
 
         # pre 2018 conditions, DOESN'T have TxRefAmp and different location for the other parameters
         else:
             if 'tsnr' in greetings and 'SAR' and 'AcquisitionTime' and 'TxRefAmp' in bids_meta:
                 product.writerow([info["date"], os.fsdecode(item)[59:],
                                   greetings['tsnr'], bids_meta["SAR"],
-                                  bids_meta["AcquisitionTime"], bids_meta['TxRefAmp'], bids_meta["SoftwareVersions"]])
+                                  bids_meta["AcquisitionTime"], bids_meta['TxRefAmp'], bids_meta["SoftwareVersions"],
+                                  bids_meta["ConversionSoftwareVersion"], bids_meta["RepetitionTime"], 
+                                  shim[0], shim[1], shim[2], shim[3], shim[4], shim[5], shim[6], shim[7]])
             else:
                 product.writerow("something went wrong")
 
