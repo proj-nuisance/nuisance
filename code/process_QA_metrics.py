05dac2116a580e2f7a2f5cc5d476d24592afd480
Christopher P. Cheng <forever.21@dartmouth.edu>
2019-02-28 15:10:16 -0500

    extracted snr_total from anat and added functionality to process_QA_metrics

--- code/process_QA_metrics.py
+++ code/process_QA_metrics.py
@@ -33,9 +33,9 @@ def get_opt_parser():
                #required=True,
                help="Where do you want the extraction to be written to"),
 
-        Option("-i", "--input",
-               dest="input", default="You didn't say where it was coming from",
-               help="Where do you want the files to be taken from?"),
+        # Option("-i", "--input",
+        #       dest="input", default="You didn't say where it was coming from",
+        #       help="Where do you want the files to be taken from?"),
 
     ])
 
@@ -57,35 +57,53 @@ def qa_metric_producer(source, output_csv):
     
     # WRITES THE HEADER ROW for the CSV
     product.writerow(["Date", "Filetype", "tsnr", "SAR", "AcquisitionTime", "TxRefAmp", "SoftwareVersions", "CSV", "RepetitionTime", 
-        "Shim1", "Shim2", "Shim3", "Shim4", "Shim5", "Shim6", "Shim7", "Shim8"])
+        "Shim1", "Shim2", "Shim3", "Shim4", "Shim5", "Shim6", "Shim7", "Shim8", "snr_total"])
 
     for item in source:  # os.listdir(os.fsencode("derivatives")):          # for each 
         info = re.search('.*/ses-(?P<date>[0-9]+)/.*', item).groupdict()
-        sesame = open(os.fsdecode(item), "r").read()  # open sesameeee
-        greetings = json.loads(sesame)
-        bids_meta = greetings["bids_meta"]
-        shim = bids_meta["ShimSetting"]
+       
+        # FUNC: abbreviations for easier access to certain dicts in the func/.json files
+        func_json = open(os.fsdecode(item), "r").read()  # open sesameeee
+        loaded_func = json.loads(func_json)
+        func_bids = loaded_func["bids_meta"]
+        shim = func_bids["ShimSetting"]
 
-        print(item); # debugging
+        # ANAT: modifying the func code to access the anatomical JSON
+        if int(info["date"]) >= 20171030:
+            anat_item = str(item)
+            anat_item = anat_item[:34] + "anat" + anat_item[38:]
+            anat_item = anat_item[:59] + "acq-MPRAGE_T1w.json"
+            
+            print(anat_item)
+            
+            anat_json = open(os.fsdecode(anat_item), "r").read()
+            loaded_anat = json.loads(anat_json) 
+
+        print(item); # debugging, indicator that a file's been processed
 
         # 2018 and later conditions
-        if 'SAR' and "AcquisitionTime" and "TxRefAmp" in greetings:
+        if 'SAR' and "AcquisitionTime" and "TxRefAmp" in loaded_func:
             product.writerow([info["date"],
-                os.fsdecode(item)[59:], greetings["tsnr"], greetings["SAR"],
-                seconds(greetings["AcquisitionTime"]), greetings["TxRefAmp"], bids_meta["SoftwareVersions"],
-                bids_meta["ConversionSoftwareVersion"], bids_meta["RepetitionTime"], 
-                shim[0], shim[1], shim[2], shim[3], shim[4], shim[5], shim[6], shim[7]])
+                os.fsdecode(item)[59:], loaded_func["tsnr"], loaded_func["SAR"],
+                seconds(loaded_func["AcquisitionTime"]), loaded_func["TxRefAmp"], func_bids["SoftwareVersions"],
+                func_bids["ConversionSoftwareVersion"], func_bids["RepetitionTime"], 
+                shim[0], shim[1], shim[2], shim[3], shim[4], shim[5], shim[6], shim[7], loaded_anat["snr_total"]])
 
         # pre 2018 conditions, DOESN'T have TxRefAmp and different location for the other parameters
         else:
-            if 'tsnr' in greetings and 'SAR' and 'AcquisitionTime' and 'TxRefAmp' in bids_meta:
-                product.writerow([info["date"], os.fsdecode(item)[59:],
-                                  greetings['tsnr'], bids_meta["SAR"],
-                                  seconds(bids_meta["AcquisitionTime"]), bids_meta['TxRefAmp'], bids_meta["SoftwareVersions"],
-                                  bids_meta["ConversionSoftwareVersion"], bids_meta["RepetitionTime"], 
-                                  shim[0], shim[1], shim[2], shim[3], shim[4], shim[5], shim[6], shim[7]])
+            if 'tsnr' in loaded_func and 'SAR' and 'AcquisitionTime' and 'TxRefAmp' in func_bids:
+                content = [info["date"], os.fsdecode(item)[59:], loaded_func['tsnr'], func_bids["SAR"],
+                    seconds(func_bids["AcquisitionTime"]), func_bids['TxRefAmp'], func_bids["SoftwareVersions"],
+                    func_bids["ConversionSoftwareVersion"], func_bids["RepetitionTime"], 
+                    shim[0], shim[1], shim[2], shim[3], shim[4], shim[5], shim[6], shim[7]] 
+                
+                if int(info["date"]) < 20171030:
+                    product.writerow(content)
+                else:
+                    content.append(loaded_anat["snr_total"])
+                    product.writerow(content)
             else:
-                product.writerow("something went wrong")
+                print("tsnr, SAR or TxRefAmp are not present.")
 
     destination.close()
 
